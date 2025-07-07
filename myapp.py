809a0f0e60a85cf514dad7a1d69bc574894b4eb8
MUHAMMAD YOUSAF RANA <21-se-58@students.uettaxila.edu.pk>
2025-07-07 18:35:03 +0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -14,7 +14,7 @@ def predict(model, image):
     resize = tf.image.resize(image, (32, 32))
     print("Resized Image Shape:", resize.shape)  # Debugging: Print resized image shape
     y_pred = model.predict(np.expand_dims(resize / 255.0, 0))
-    if y_pred < 0.50:
+    if y_pred < 0.40:
         return "REAL IMAGE"
     else:
         return "AI GENERATED IMAGE"
